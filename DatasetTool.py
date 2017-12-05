b959c8e31b16562d5e10a3a4231289ffc42b05a4
Krasus <mingxuan.krasus@gmail.com>
2017-12-04 22:34:49 -0600

    runnable prototype

--- DatasetTool.py
+++ DatasetTool.py
@@ -1,54 +1,62 @@
 from os import chdir
+import numpy as np
 
-class DatasetTool:
-    def __init__(self, dataset_config_path, batch_size):
-    '''
-    dataset_config_path is the full path of dataset_config_file
-    dataset_directory should looks like:
-        -database_directory
-            -subject1
-                -anger
-                    -sentence1
-                        -noise1: this directory contains Timestep number of audio segments
+class DatasetTool(object):
+    def __init__(self, dataset_config_path, batch_size, timesteps):
+        '''
+        dataset_config_path is the full path of dataset_config_file
+        dataset_directory should looks like:
+            -database_directory
+                -subject1
+                    -anger
+                        -sentence1
+                            -noise1: this directory contains Timestep number of audio segments
+                            ...
+                            -noise20
                         ...
-                        -noise20
+                        -sentence5
+                    -disgust
                     ...
-                    -sentence5
-                -disgust
+                    -surprise
+                -subject2
                 ...
-                -surprise
-            -subject2
+                -subjectx
+
+        dataset_config_file should contain lines like below:
+            sentence_path1 ground_truth1
+            sentence_path2 ground_truth2
             ...
-            -subjectx
+            sentence_path_n ground_truth_n
 
-    dataset_config_file should contain lines like below:
-        sentence_path1 ground_truth1
-        sentence_path2 ground_truth2
-        ...
-        sentence_path_n ground_truth_n
-
-    CAUTION: the sequence of the list above should be random first! And it's sentence_path but audio_segment_path.
-    At some arbitary ratio, the list above will be separated as train and test set in this __init__ function.
-
-    '''
-
+        CAUTION: the sequence of the list above should be random first! And it's sentence_path but audio_segment_path.
+        At some arbitary ratio, the list above will be separated as train and test set in this __init__ function.
+        '''
+        self.batch_size = batch_size
+        self.timesteps = timesteps
+        return
 
     def next_train_batch(self):
-    '''
-    return batch_x, batch_y
-    batch_x = [[3s frame * Timestep] * batch_size]
-    batch_y = [ground_truth * batch_size]
-    ground_truth is the following number symbols:
-        0 - anger
-        1 - disgust
-        2 - fear
-        3 - happiness
-        4 - sadness
-        5 - surprise
-    '''
+        '''
+        return batch_x, batch_y
+        batch_x = [[3s frame * Timestep] * batch_size]
+        batch_y = [ground_truth * batch_size]
+        ground_truth is the following number symbols:
+            0 - anger
+            1 - disgust
+            2 - fear
+            3 - happiness
+            4 - sadness
+            5 - surprise
+        '''
+        return np.array([[np.zeros((400, 300, 3)) for __ in range(self.timesteps)] for _ in xrange(self.batch_size)]),\
+          np.array([[np.zeros((20)) for __ in range(self.timesteps)] for _ in xrange(self.batch_size)]),\
+          np.array([[1, 0, 0, 0, 0, 0] for _ in xrange(self.batch_size)])
 
     def next_test_batch(self):
-    '''
-    return test_batch_x, test_batch_y
-    format is the same with next_train_batch
-    '''
+        '''
+        return test_batch_x, test_batch_y
+        format is the same with next_train_batch
+        '''
+        return np.array([[np.zeros((400, 300, 3)) for __ in range(self.timesteps)] for _ in xrange(self.batch_size)]),\
+          np.array([[np.zeros((20)) for __ in range(self.timesteps)] for _ in xrange(self.batch_size)]),\
+          np.array([[1, 0, 0, 0, 0, 0] for _ in xrange(self.batch_size)])
--- EmotionRecognition.py
+++ EmotionRecognition.py
@@ -4,28 +4,28 @@ import tensorflow as tf
 from tensorflow.contrib import rnn
 import numpy as np
 
-import DatasetTool
+from DatasetTool import DatasetTool
 
 # Training Parameters
 learning_rate = 0.001
 training_epoch = 100
 training_steps = 10000
 testing_steps = 100
-batch_size = 16
-display_step = 200
+batch_size = 1
+display_step = 1
 dataset_config_file_path = ''
 dropout_rate = 0.3
-
-dataset = DatasetTool(dataset_config_file_path, batch_size)
 
 # Network Parameters
 input_x = 400
 input_y = 300
 input_z = 3
 input_1 = 20 # extra vector size example
-timesteps = 5 # timesteps
+timesteps = 2 # timesteps
 num_hidden = 128 # hidden layer num of features
 num_classes = 6 # total classes (0-9 digits)
+
+dataset = DatasetTool(dataset_config_file_path, batch_size, timesteps)
 
 # tf Graph input
 X = tf.placeholder("float", [batch_size, timesteps, input_x, input_y, input_z])
@@ -39,9 +39,9 @@ weights = {
     # 8x12 conv, 16 inputs, 24 outputs
     'wconv2': tf.Variable(tf.random_normal([8, 12, 16, 24])),
     # 5x7 conv, 24 inputs, 32 outputs
-    'wconv2': tf.Variable(tf.random_normal([5, 7, 24, 32])),
+    'wconv3': tf.Variable(tf.random_normal([5, 7, 24, 32])),
     # fully connected, 7*7*64 inputs, 1024 outputs
-    'wfc1': tf.Variable(tf.random_normal([100, 128])),#TODO vector size tbd, the number should be conv3 + x1
+    'wfc1': tf.Variable(tf.random_normal([60820, 128])),#TODO vector size tbd, the number should be conv3 + x1
     # 1024 inputs, 10 outputs (class prediction)
     'classify': tf.Variable(tf.random_normal([2*num_hidden, num_classes]))
 }
@@ -49,7 +49,7 @@ biases = {
     'bconv1': tf.Variable(tf.random_normal([16])),
     'bconv2': tf.Variable(tf.random_normal([24])),
     'bconv3': tf.Variable(tf.random_normal([32])),
-    'bd1': tf.Variable(tf.random_normal([128])),
+    'bfc1': tf.Variable(tf.random_normal([128])),
     'classify': tf.Variable(tf.random_normal([num_classes]))
 }
 
@@ -62,9 +62,14 @@ def maxpool2d(x, k=2):
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
 
 def convNet(x, x1):
+    # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
+    x = tf.reshape(x, shape=[-1, input_x, input_y, input_z])
+    x1 = tf.reshape(x1, shape=[-1, input_1])
     # Convolution Layer
     conv1 = conv2d(x, weights['wconv1'], biases['bconv1'])
     conv1 = maxpool2d(conv1, k=2)
+
+    print('conv1_shape:', tf.shape(conv1))
 
     conv2 = conv2d(conv1, weights['wconv2'], biases['bconv2'])
     conv2 = maxpool2d(conv2, k=2)
@@ -74,9 +79,9 @@ def convNet(x, x1):
 
     # Fully connected layer
     # Reshape conv2 output to fit fully connected layer input
-    conv3 = tf.reshape(conv3, [batch_size*timesteps, -1])
+    conv3 = tf.reshape(conv3, [-1, 50 * 38 * 32])
     conv3 = tf.concat([conv3, x1], 1)
-    #fc1 = tf.reshape(conv3, [-1, weights['wfc1'].get_shape().as_list()[0]])
+
     fc1 = tf.add(tf.matmul(conv3, weights['wfc1']), biases['bfc1'])
     fc1 = tf.nn.relu(fc1)
     # Apply Dropout
@@ -86,6 +91,7 @@ def convNet(x, x1):
 
 
 def BiRNN(x):
+    x = tf.reshape(x, [batch_size, timesteps, -1])
     x = tf.unstack(x, timesteps, 1)
     # Define lstm cells with tensorflow
     # Forward direction cell
@@ -95,26 +101,17 @@ def BiRNN(x):
     # Get lstm cell output
     outputs, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x, dtype=tf.float32)
 
-    return outputs
-
-def classifyNet(inputs):
-    # Linear activation, using rnn inner loop last output
-    logits =  tf.matmul(inputs[-1], weights['classify']) + biases['classify']
-    return tf.nn.softmax(logits)
-
+    logits =  tf.matmul(outputs[-1], weights['classify']) + biases['classify']
+    return logits
 
 #Full NN architecture
-X_reshape = tf.reshape(X, [batch_size * timesteps, input_x, input_y, input_z]) #unpack timesteps*batch_size
-X_1_reshape = tf.reshape(X_1, [batch_size * timesteps, input_1])
-
-outputs = convNet(X_reshape, X_1_reshape)
+outputs = convNet(X, X_1)
 
 logits = BiRNN(outputs)
 prediction = tf.nn.softmax(logits)
 
 # Define loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-    logits=logits, labels=Y))
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
 
@@ -133,24 +130,25 @@ with tf.Session() as sess:
 
     for epoch_num in range(1, training_epoch + 1):
         for step in range(1, training_steps + 1):
-            batch_x, batch_y = dataset.next_train_batch()#TODO batch_x should be diveided into x and x_1
-            sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+            batch_x, batch_x_1, batch_y = dataset.next_train_batch()
+            #print(batch_x, batch_x_1, batch_y)
+            #print(batch_x_1.shape)
+            sess.run(train_op, feed_dict={X: batch_x, X_1: batch_x_1, Y: batch_y})
             if step % display_step == 0 or step == 1:
                 # Calculate batch loss and accuracy
-                loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
-                                                                     Y: batch_y})
+                loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x, X_1: batch_x_1, Y: batch_y})
                 print("Training Step " + str(step) + ", Minibatch Loss= " + \
                       "{:.4f}".format(loss) + ", Training Accuracy= " + \
                       "{:.3f}".format(acc))
 
-        if epoch_num % 5 == 0: #test every 5 epoch
-            for step in range(1, testing_steps + 1):
-                batch_x, batch_y = dataset.next_test_batch()
-                sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
-                if step % display_step == 0 or step == 1:
-                    # Calculate batch loss and accuracy
-                    loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
-                                                                         Y: batch_y})
-                    print("Testing Step " + str(step) + ", Minibatch Loss= " + \
-                          "{:.4f}".format(loss) + ", Training Accuracy= " + \
-                          "{:.3f}".format(acc))
+        # if epoch_num % 5 == 0: #test every 5 epoch
+        #     for step in range(1, testing_steps + 1):
+        #         batch_x, batch_y = dataset.next_test_batch()
+        #         sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+        #         if step % display_step == 0 or step == 1:
+        #             # Calculate batch loss and accuracy
+        #             loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
+        #                                                                  Y: batch_y})
+        #             print("Testing Step " + str(step) + ", Minibatch Loss= " + \
+        #                   "{:.4f}".format(loss) + ", Training Accuracy= " + \
+        #                   "{:.3f}".format(acc))
